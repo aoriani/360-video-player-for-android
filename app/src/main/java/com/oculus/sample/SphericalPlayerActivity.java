2d7c5484536c26682be137ec80c36b01e0cddd80
Andre Oriani <aoriani@gmail.com>
2016-11-19 23:04:27 -0800

    Immersive Model

--- app/src/main/java/com/oculus/sample/SphericalPlayerActivity.java
+++ app/src/main/java/com/oculus/sample/SphericalPlayerActivity.java
@@ -140,4 +140,17 @@ public class SphericalPlayerActivity extends AppCompatActivity {
         }
         return text.toString();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 }
